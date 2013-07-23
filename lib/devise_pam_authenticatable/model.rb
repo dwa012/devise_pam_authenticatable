4672eefe0848b260f41bee30a6094e20f2a70008
Daniel Ward <dwa012@gmail.com>
2013-07-23 11:12:33 -0500

    remvoed saving of password and changed username to email

--- lib/devise_pam_authenticatable/model.rb
+++ lib/devise_pam_authenticatable/model.rb
@@ -19,18 +19,18 @@ module Devise
 
      # Checks if a resource is valid upon authentication.
      def valid_pam_authentication?(password)
-       Devise::PamAdapter.valid_credentials?(self.username, password)
+       Devise::PamAdapter.valid_credentials?(self.email, password)
      end
      
      module ClassMethods
        def authenticate_with_pam(attributes={})
-         return nil unless attributes[:username].present?
+         return nil unless attributes[:email].present?
 
-         resource = scoped.where(:username => attributes[:username]).first
+         resource = scoped.where(:email => attributes[:email]).first
          if resource.blank?
            resource = new
-           resource[:username] = attributes[:username]
-           resource[:password] = attributes[:password]
+           resource[:email] = attributes[:email]
+           #resource[:password] = attributes[:password]
          end
 
          if resource.try(:valid_pam_authentication?, attributes[:password])
